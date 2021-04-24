72c8a04708f1f41c29633090bd914ee266d0422c
Eloy Lang <thisiseloy@gmail.com>
2021-04-24 10:29:50 -0700

    Workshop Week 4 | Comment Button/Modal/Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Card, CardImg, CardText,  CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component, Fragment }  from 'react';
+import { Card, CardImg, CardText,  CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
     return (
@@ -9,12 +13,91 @@ function RenderCampsite({campsite}) {
                 <CardImg top src={campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardBody>
-                        <CardText>{campsite.description}</CardText>
+                            <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </CardBody>
             </Card>
         </div>
     )
+}
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          isModalOpen: false,
+          rating: '1',
+          author: '',
+          text: '',
+          touched: {
+              author: false,
+          }
+
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleComment = this.handleComment.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleComment(values) {
+        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <Button outline onClick={this.toggleModal}><i className="fa fa-pencil fa-lg" /> Submit Comment</Button>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleComment}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                    <Control.text model=".author" name="author" id="author" className="form-control" placeholder="Your Name"
+                                    validators={{
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }} />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: "Name must be at least 2 characters",
+                                            maxLength: "Name must be 15 characters or less"
+                                        }}
+                                    />
+                                    </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea model=".text" id="text" name="text" rows="6" className="form-control" />
+                            </div>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </Fragment>
+        );
+    }
 }
 
 function RenderComments({comments}) {
@@ -29,6 +112,7 @@ function RenderComments({comments}) {
                         -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </div>)}
+                <CommentForm />
             </div>
         )
     }
